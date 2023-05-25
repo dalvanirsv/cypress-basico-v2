78832f315af4586b3df4eaa44028eede8cd3d6a1
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-05-25 00:22:06 -0300

    add-new-it

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,12 +1,19 @@
-import cypress from "cypress"
-
-describe('central de atendimento ao cliente TAT', function(){
+describe('central de Atendimento ao Cliente TAT', function(){
     beforeEach(function() {
         cy.visit('./src/index.html')
     })
         
         it('verifica o título da aplicação', function(){
             cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preencha os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('dalvanir')
+        cy.get('#lastName').type('vieira')
+        cy.get('#email').type('dalvanir@dalvanir')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click
+
     })
 })
 
