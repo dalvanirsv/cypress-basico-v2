a26fbb3249cf0d49c85770de57146e4d6efc98f5
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-05-24 23:49:58 -0300

    add-beforeeach

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,12 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+import cypress from "cypress"
+
+describe('central de atendimento ao cliente TAT', function(){
+    beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
+        
+        it('verifica o título da aplicação', function(){
+            cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+})
+
