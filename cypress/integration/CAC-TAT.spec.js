cca5c52621bea8ee98dde322ad63c4d889f1a974
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-06-23 00:19:29 -0300

    format-cod

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,153 +1,139 @@
+describe("central de Atendimento ao Cliente TAT", function () {
+  beforeEach(function () {
+    cy.visit("./src/index.html");
+  });
 
-describe('central de Atendimento ao Cliente TAT', function(){
-    beforeEach(function() {
-        cy.visit('./src/index.html')
-    })
-        
-        it('verifica o título da aplicação', function(){
-            cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-    })
+  it("verifica o título da aplicação", function () {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
 
-    it('preencha os campos obrigatórios e envia o formulário', function(){
-        const longTest = 'teste, teste, teste, teste, teste, teste,teste, teste, teste,teste, teste, teste,teste, teste, teste'
-        cy.get('#firstName').type('dalvanir')
-        cy.get('#lastName').type('vieira')
-        cy.get('#email').type('dalvanir@dalvanir.com')
-        cy.get('#open-text-area').type(longTest, { delay: 0})
-        cy.contains('button', 'Enviar').click()
-        cy.get('.success').should('be.visible')
+  it("preencha os campos obrigatórios e envia o formulário", function () {
+    const longTest =
+      "teste, teste, teste, teste, teste, teste,teste, teste, teste,teste, teste, teste,teste, teste, teste";
+    cy.get("#firstName").type("dalvanir");
+    cy.get("#lastName").type("vieira");
+    cy.get("#email").type("dalvanir@dalvanir.com");
+    cy.get("#open-text-area").type(longTest, { delay: 0 });
+    cy.contains("button", "Enviar").click();
+    cy.get(".success").should("be.visible");
+  });
 
-    })
+  it("exibe mensagem de erro ao submeter o formulário com um email com  formação", function () {
+    cy.get("#firstName").type("dalvanir");
+    cy.get("#lastName").type("vieira");
+    cy.get("#email").type("dalvanir@dalvanir");
+    cy.get("#open-text-area").type("test");
+    cy.contains("button", "Enviar").click();
+    cy.get(".error").should("be.visible");
+  });
 
-    it('exibe mensagem de erro ao submeter o formulário com um email com  formação',function(){
-        cy.get('#firstName').type('dalvanir')
-        cy.get('#lastName').type('vieira')
-        cy.get('#email').type('dalvanir@dalvanir')
-        cy.get('#open-text-area').type('test')
-        cy.contains('button', 'Enviar').click()
-        cy.get('.error').should('be.visible')
-    })
+  it("Campo telefone continua vazio quando preenchido com valor não-numérico", function () {
+    cy.get("#phone").type("fsdkfmkdsf").should("have.value", "");
+  });
 
-    it('Campo telefone continua vazio quando preenchido com valor não-numérico', function(){
-        cy.get('#phone')
-        .type('fsdkfmkdsf')
-        .should('have.value','')
-    })
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", function () {
+    cy.get("#firstName").type("dalvanir");
+    cy.get("#lastName").type("vieira");
+    cy.get("#email").type("dalvanir@dalvanir.com");
+    cy.get("#phone-checkbox").click();
+    cy.get("#open-text-area").type("test");
+    cy.contains("button", "Enviar").click();
+    cy.get(".error").should("be.visible");
+  });
 
-    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
-        cy.get('#firstName').type('dalvanir')
-        cy.get('#lastName').type('vieira')
-        cy.get('#email').type('dalvanir@dalvanir.com')
-        cy.get('#phone-checkbox').click()
-        cy.get('#open-text-area').type('test')
-        cy.contains('button', 'Enviar').click()
-        cy.get('.error').should('be.visible')
-    })
+  it("preencha e limpa os campos nome, sobrenome, email e telefone", function () {
+    cy.get("#firstName")
+      .type("dalvanir")
+      .should("have.value", "dalvanir")
+      .clear()
+      .should("have.value", "");
+    cy.get("#lastName")
+      .type("vieira")
+      .should("have.value", "vieira")
+      .clear()
+      .should("have.value", "");
+    cy.get("#email")
+      .type("dalvanir@dalvanir.com")
+      .should("have.value", "dalvanir@dalvanir.com")
+      .clear()
+      .should("have.value", "");
+    cy.get("#open-text-area")
+      .type("teste")
+      .should("have.value", "teste")
+      .clear()
+      .should("have.value", "");
+  });
 
-    it('preencha e limpa os campos nome, sobrenome, email e telefone', function() {
-        cy.get('#firstName')
-        .type('dalvanir')
-        .should('have.value', 'dalvanir')
-        .clear()
-        .should('have.value', '')
-        cy.get('#lastName')
-        .type('vieira')
-        .should('have.value', 'vieira')
-        .clear()
-        .should('have.value', '')
-        cy.get('#email')
-        .type('dalvanir@dalvanir.com')
-        .should('have.value', 'dalvanir@dalvanir.com')
-        .clear()
-        .should('have.value', '')
-        cy.get('#open-text-area')
-        .type('teste')
-        .should('have.value', 'teste')
-        .clear()
-        .should('have.value', '')
+  it("validar mensagem de erro ao não digitar os campos obrigatórios", function () {
+    cy.get('button[type="submit"]').click();
 
-    })
+    cy.get(".error").should("be.visible");
+  });
 
-    it('validar mensagem de erro ao não digitar os campos obrigatórios', function() {
-        cy.get('button[type="submit"]').click()
+  it("envia um formulário com sucesso usando um comando customizado", function () {
+    cy.fillMandatoryFieldsAndSubmit();
 
-        cy.get('.error').should('be.visible')
+    cy.get(".success").should("be.visible");
+  });
 
-    })   
+  it('seleciona um produto "youtube" por seu texto', function () {
+    cy.get("#product").select("YouTube");
+  });
 
-    it('envia um formulário com sucesso usando um comando customizado', function() {
-        cy.fillMandatoryFieldsAndSubmit()
+  it('seleciona um produto "Mentoria" por seu Valor (value)', function () {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
+  });
 
-        cy.get('.success').should('be.visible')
-    })   
+  it('seleciona um produto "Blog" por seu Indice', function () {
+    cy.get("#product").select("1").should("have.value", "blog");
+  });
 
-    it('seleciona um produto "youtube" por seu texto', function() {
-        cy.get('#product').select('YouTube')
+  it('Marca um tipo de atendimento "feedback"', function () {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should("have.value", "feedback");
+  });
 
-    }) 
+  it("Marca cada tipo de atendimento ", function () {
+    cy.get('input[type="radio"]')
+      .should("have.length", 3)
+      .each(function ($radio) {
+        cy.wrap($radio).check();
+        cy.wrap($radio).should("be.checked");
+      });
+  });
+  it("Marca ambos os checkbox, e depois desmarca o último ", function () {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .last()
+      .uncheck()
+      .should("not.be.checked");
+  });
 
-    it('seleciona um produto "Mentoria" por seu Valor (value)', function() {
-        cy.get('#product')
-        .select('mentoria')
-        .should('have.value', 'mentoria')
+  it("Seleciona um arquivo da pasta fixtures", function () {
+    cy.get('input[type="file"]#file-upload')
+      .should("not.have.value")
 
-    }) 
+      .selectFile("./cypress/fixtures/example.json")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+  it("Seleciona um arquivo simulando um drag-and-drog", function () {
+    cy.get('input[type="file"]#file-upload')
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json", { action: "drag-drop" })
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", function () {
+    cy.get("#privacy a").should("have.attr", "target", "_blank");
+  });
 
-    it('seleciona um produto "Blog" por seu Indice', function() {
-        cy.get('#product')
-        .select('1')
-        .should('have.value', 'blog')
+  it("Acessa a página da política de privacidade removendo o o target e então clicando no link", function () {
+    cy.get("#privacy a").invoke("removeAttr", "target").click();
 
-    }) 
-
-    it('Marca um tipo de atendimento "feedback"', function() {
-        cy.get('input[type="radio"][value="feedback"]')
-        .check()
-        .should('have.value', 'feedback')
-    }) 
-
-    it('Marca cada tipo de atendimento ', function() {
-        cy.get('input[type="radio"]')
-        .should('have.length',3)
-        .each(function($radio){
-            cy.wrap($radio).check()
-            cy.wrap($radio).should('be.checked')
-        })
-    })
-        it('Marca ambos os checkbox, e depois desmarca o último ', function() {
-           cy.get('input[type="checkbox"]')
-           .check()
-           .last()
-           .uncheck()
-           .should('not.be.checked')
-            
-    }) 
-
-    it('Seleciona um arquivo da pasta fixtures', function() {
-       cy.get('input[type="file"]#file-upload')
-         .should('not.have.value')
-         .selectFile('./cypress/fixtures/example.json')
-         .should(function($input){
-            expect($input[0].files[0].name).to.equal('example.json')
-         })
-        })
-         it('Seleciona um arquivo simulando um drag-and-drog', function() {
-            cy.get('input[type="file"]#file-upload')
-              .should('not.have.value')
-              .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
-              .should(function($input){
-                 expect($input[0].files[0].name).to.equal('example.json')
-              })
-    }) 
-    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
-        cy.get('#privacy a').should('have.attr','target', '_blank')
-          })
-
-          it.only('Acessa a página da política de privacidade removendo o o target e então clicando no link', function() {
-            cy.get('#privacy a')
-            .invoke('removeAttr', 'target')
-            .click()
-
-            cy.contains('Talking About Testing').should('be.visible')
-              })
-})
+    cy.contains("Talking About Testing").should("be.visible");
+  });
+});
